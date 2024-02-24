d6c9d2de3df4687db96deb68feb690857d46113e
shihao <2425327896@qq.com>
2024-02-24 08:41:21 +0800

    4444

--- src/main/java/com/atguigu/MyApplication.java
+++ src/main/java/com/atguigu/MyApplication.java
@@ -11,6 +11,7 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 public class MyApplication {
     public static void main(String[] args) {
         System.out.println("1111");
+        System.out.println("4444");
         System.out.println("i am master");
         System.out.println("i am hotfix");
         SpringApplication.run(MyApplication.class, args);
