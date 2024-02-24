1fb87c490dc9068fd4e0917b4b9029087b172174
shihao <2425327896@qq.com>
2024-02-24 08:45:09 +0800

    3333

--- src/main/java/com/atguigu/MyApplication.java
+++ src/main/java/com/atguigu/MyApplication.java
@@ -12,6 +12,7 @@ public class MyApplication {
     public static void main(String[] args) {
         System.out.println("1111");
         System.out.println("2222");
+        System.out.println("3333");
         System.out.println("i am master");
         System.out.println("i am hotfix");
         SpringApplication.run(MyApplication.class, args);
